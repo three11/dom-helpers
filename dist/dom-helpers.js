98b8a2674f447ffa1adb6155e77c67925af43a07
Atanas Atanasov <scriptex.bg@gmail.com>
2018-09-18 08:11:07 +0300

    Update dist

--- dist/dom-helpers.js
+++ dist/dom-helpers.js
@@ -1,198 +1,107 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["AnimateMe"] = factory();
-	else
-		root["AnimateMe"] = factory();
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.domHelpers = mod.exports;
+  }
+})(this, function (_exports) {
+  "use strict";
 
-"use strict";
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.enableListeners = _exports.$$ = _exports.$ = void 0;
 
+  var _this = void 0;
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.enableListeners = exports.$$ = exports.$ = void 0;
+  function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
-var _this = void 0;
+  function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+  function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
 
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+  function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
-
-var D = document;
-/**
- * Query a single element
- */
-// prettier-ignore
-
-var $ = function $(selector) {
-  var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : D;
-  return context.querySelector(selector);
-};
-/**
- * Query a collection of element
- */
-// prettier-ignore
-
-
-exports.$ = $;
-
-var $$ = function $$(selector) {
-  var startNode = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : D;
-  return _toConsumableArray(startNode.querySelectorAll(selector));
-};
-
-exports.$$ = $$;
-
-var enableListeners = function enableListeners() {
+  var D = document;
   /**
-   * Add `on` method to Element
-   *
-   * @param {String} type
-   * @param {Function} listener
-   * @param {Object|Boolean} options
+   * Query a single element
    */
-  Element.prototype.on = function (type, listener) {
-    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+  // prettier-ignore
 
-    _this.addEventListener(type, listener, options);
+  var $ = function $(selector) {
+    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : D;
+    return context.querySelector(selector);
   };
   /**
-   * Add `on` method to Array
-   *
-   * @param {String} type
-   * @param {Function} listener
-   * @param {Object|Boolean} options
+   * Query a collection of element
    */
-  // eslint-disable-next-line no-extend-native
+  // prettier-ignore
 
 
-  Array.prototype.on = function (type, listener) {
-    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+  _exports.$ = $;
 
-    _this.map(function (el) {
-      if (el instanceof Element) {
-        el.addEventListener(type, listener, options);
-      }
-    });
+  var $$ = function $$(selector) {
+    var startNode = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : D;
+    return _toConsumableArray(startNode.querySelectorAll(selector));
   };
-};
-/**
- * Usage
- *
- * const button = $('#button');
- *
- * button.on('click', () => {
- *     console.log('clicked a single button');
- * });
- *
- * const buttons = $$('.button');
- *
- * buttons.on('click', () => {
- *     console.log('clicked a button in a collection');
- * });
- */
+
+  _exports.$$ = $$;
+
+  var enableListeners = function enableListeners() {
+    /**
+     * Add `on` method to Element
+     *
+     * @param {String} type
+     * @param {Function} listener
+     * @param {Object|Boolean} options
+     */
+    Element.prototype.on = function (type, listener) {
+      var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+      _this.addEventListener(type, listener, options);
+    };
+    /**
+     * Add `on` method to Array
+     *
+     * @param {String} type
+     * @param {Function} listener
+     * @param {Object|Boolean} options
+     */
+    // eslint-disable-next-line no-extend-native
 
 
-exports.enableListeners = enableListeners;
+    Array.prototype.on = function (type, listener) {
+      var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
 
-/***/ })
-/******/ ]);
+      _this.map(function (el) {
+        if (el instanceof Element) {
+          el.addEventListener(type, listener, options);
+        }
+      });
+    };
+  };
+  /**
+   * Usage
+   *
+   * const button = $('#button');
+   *
+   * button.on('click', () => {
+   *     console.log('clicked a single button');
+   * });
+   *
+   * const buttons = $$('.button');
+   *
+   * buttons.on('click', () => {
+   *     console.log('clicked a button in a collection');
+   * });
+   */
+
+
+  _exports.enableListeners = enableListeners;
 });
